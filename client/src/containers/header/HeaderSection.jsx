f2e8aa4d836f483dda147fdf543164f5a1fa7d2d
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-15 13:49:24 +0100

    good

--- client/src/containers/header/HeaderSection.jsx
+++ client/src/containers/header/HeaderSection.jsx
@@ -1,21 +1,21 @@
-import './header.css';
-import assets from '../../assets';
-import Brand from '../../components/brand/Brand';
-import About from '../about/About'; 
-import Features from '../features/Features';
-import Possibility from '../possibility/Possibility';
-import Blog from '../blog/Blog';
-import Contact from '../contact/Contact';
-import Cta from  '../../components/cta/Cta';
-import { Link, NavLink } from 'react-router-dom';
-import NavbarMainHome from "../../components/navbar/NavbarMainHome"
-import lines1 from "../../assets/linesImage1.png"
-import lines2 from "../../assets/layer-slider-line-2-removebg-preview.png"
-
+import "./header.css";
+import assets from "../../assets";
+import Brand from "../../components/brand/Brand";
+import About from "../about/About";
+import Features from "../features/Features";
+import Possibility from "../possibility/Possibility";
+import Blog from "../blog/Blog";
+import Contact from "../contact/Contact";
+import Cta from "../../components/cta/Cta";
+import { Link, NavLink } from "react-router-dom";
+import NavbarMainHome from "../../components/navbar/NavbarMainHome";
+import lines1 from "../../assets/linesImage1.png";
+import lines2 from "../../assets/layer-slider-line-2-removebg-preview.png";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Header = () => (
   <>
-          {/* <NavbarMainHome /> */}
+    {/* <NavbarMainHome /> */}
     {/* <div className="bestowal__header section__padding" id="home">
       <div className="bestowal__header-content">
         <h1 className="gradient__text">Let&apos;s us know your project idea that you have and bring it to live</h1>
@@ -42,19 +42,31 @@ const Header = () => (
   <Cta />
   <Blog />
   <Contact /> */}
-  
-  <section className='headerSections'>
-    <div className='headerImage'>
-      <h1 className='earlyAccess'>Get early access to the ideas of tomorrow</h1>
-      <p className='industryLeading'>Highly-vetted Web3 projects you can trust. Supported by industry-leading creators and funds.</p>
-      
-      <NavLink>
-        
-      </NavLink>
-    </div>
-  </section>
-</>
 
+    <section className="headerSections">
+      <div className="headerImage">
+        <h1 className="earlyAccess">
+          Get early access to the ideas of tomorrow
+        </h1>
+        <p className="industryLeading">
+          Highly-vetted Web3 projects you can trust. Supported by
+          industry-leading creators and funds.
+        </p>
+
+        <NavLink to="/hero" className="myHeroPart">
+          Go to project{" "}
+          <span>
+            <FaArrowRight />
+          </span>
+        </NavLink>
+
+        <img src={lines1} alt="lines1" className="theHeaderImage" />
+
+        <img src={lines2} alt="lines1" className="theHeaderImage2" />
+      </div>
+    </section>
+    <Brand />
+  </>
 );
 
 export default Header;
