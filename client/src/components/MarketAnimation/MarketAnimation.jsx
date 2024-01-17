db232c97bd865951c3ba2568658ea3c62daf154c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-17 14:54:53 +0100

    great job

---
+++ client/src/components/MarketAnimation/MarketAnimation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./MarketAnimation.css"
+
+const MarketAnimation = () => {
+    return <div className="marketAnimation"></div>;
+}
+
+export default MarketAnimation
+
--- client/src/components/MarketRange/MarketRange.jsx
+++ client/src/components/MarketRange/MarketRange.jsx
@@ -27,3 +27,6 @@ const MarketRange = () => {
 };
 
 export default MarketRange;
+
+
+
