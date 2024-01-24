0a4462be87481f9d4727e0d9968f9ce4cdfb8df6
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-24 19:35:19 +0100

    helllo

--- client/src/containers/Liquidity/Liquidity.jsx
+++ client/src/containers/Liquidity/Liquidity.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import "./liquidity.css"
+import React from "react";
+import "./liquidity.css";
+import { NavLink } from "react-router-dom";
 
 const liquidity = () => {
   return (
-    <section className='liquidity'>Liquidity</section>
-  )
-}
+    <section className="liquidity">
+      <div className="liquidityHeader">
+        <div className="knowledge">
+          <div className="knowledge-content">
+            <h1>Zero knowledge required</h1>
+            <p>
+              Scroll’s mission is to provide an accessible scaling solution that
+              preserves the essence of Ethereum – trust-minimized, secure and
+              open source. Like a scroll, our story is constantly evolving in
+              our quest to secure Ethereum’s future and making the developer
+              experience as easy as possible.
+            </p>
+          </div>
+          <div className="buttonMaker">
+            <button>
+              <NavLink>Hello making</NavLink>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default liquidity
+export default liquidity;
