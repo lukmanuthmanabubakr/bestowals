54fc313ed953bb8d6bd0587f1f8057ab5f0576f1
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-26 22:25:09 +0100

    good

--- client/src/containers/Liquidity/Liquidity.jsx
+++ client/src/containers/Liquidity/Liquidity.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./liquidity.css";
 import { NavLink } from "react-router-dom";
 
+import pest_1 from "../../assets/pest_1.jpeg";
+import pest_2 from "../../assets/pest__2.jpeg";
+import pest_3 from "../../assets/pest_3.jpeg";
+
 const liquidity = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   return (
     <section className="liquidity">
       <div className="liquidityHeader" data-aos="fade-right">
@@ -21,6 +38,47 @@ const liquidity = () => {
             <button>
               <NavLink>Hello making</NavLink>
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="card-group">
+        <div className="card">
+          <img src={pest_1} alt="pest_1 " />
+          <div className="layer"></div>
+          <div className="info">
+            <h1>SingaPore</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              eveniet pariatur harum excepturi amet saepe placeat, eaque
+              officiis dignissimos beatae?
+            </p>
+            <button>Explore</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={pest_2} alt="pest_1 " />
+          <div className="layer"></div>
+          <div className="info">
+            <h1>SingaPore</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              eveniet pariatur harum excepturi amet saepe placeat, eaque
+              officiis dignissimos beatae?
+            </p>
+            <button>Explore</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={pest_3} alt="pest_1 " />
+          <div className="layer"></div>
+          <div className="info">
+            <h1>SingaPore</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              eveniet pariatur harum excepturi amet saepe placeat, eaque
+              officiis dignissimos beatae?
+            </p>
+            <button>Explore</button>
           </div>
         </div>
       </div>
