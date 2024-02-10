6d2d6577a2149a271ffb015e655d6c383b0c0aa7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-10 22:55:38 +0100

    cool

--- client/src/containers/possibility/Possibility.jsx
+++ client/src/containers/possibility/Possibility.jsx
@@ -9,16 +9,15 @@ const Possibility = () => (
         <img src={Painting} alt="possibility" />
       </div>
       <div className="bestowal__possibility-content">
-        <h4>Request Early Access to Get Started</h4>
-        <h1 className="gradient__text">
-          The possibilities are <br /> beyond your imagination
+        <h4>Request Early Access<br /> to Get Started</h4>
+        <h1>
+          The possibilities are  beyond your imagination
         </h1>
         <p>
           Yet bed any for assistance indulgence unpleasing. Not thoughts all
           exercise blessing. Indulgence way everything joy alteration boisterous
           the attachment.
         </p>
-        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   </section>
