20b57f6d2533e8b211dad2c6afe618f053b8efb9
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-12 15:28:56 +0100

    goddd

--- client/src/containers/Creativity/Creativity.jsx
+++ client/src/containers/Creativity/Creativity.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Creativity.css";
 
 const Creativity = () => {
   return (
-    <section>Creativity</section>
-  )
-}
+    <section className="creativity-section">
+      <div>
+        <p>Creativity</p>
+      </div>
+      <div className="about-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="about-container">
+      <h4>Creativity</h4>
+      <p>The use of imagination or original ideas to create something firm are keen to encourage.</p>
+      <h4>Knowledge</h4>
+      <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+      <h4>Education</h4>
+      <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+      </div>
+    </section>
+  );
+};
 
-export default Creativity
+export default Creativity;
