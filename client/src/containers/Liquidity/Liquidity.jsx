b7c2d9c6ef2a415ee3bd666499d5fa8806c79fc0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-26 13:23:13 +0100

    update

--- client/src/containers/Liquidity/Liquidity.jsx
+++ client/src/containers/Liquidity/Liquidity.jsx
@@ -36,6 +36,10 @@ const liquidity = () => {
 
      <div className="liquidityAspectTwo">
         <h1 className="possibleHead">Possibilities</h1>
+        <div>
+          <p>The possibilities are beyond your imagination</p>
+          <p>Explore the Library</p>
+        </div>
      </div>
     </section>
   );
